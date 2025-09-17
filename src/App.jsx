5a849e92fb7cb495f2e6683a70d2940a670c3d9d
JuliusBourbon <naherrrrr@gmail.com>
2025-09-17 18:36:17 +0700

    Add: Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Routes, Route, useLocation  } from 'react-router-dom'
 import './App.css'
+import LandingPage from './components/landingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-red-500 text-center'>
-      Tes
-    </div>
+    <Routes>
+      <Route path='/' element={<LandingPage/>}></Route>
+    </Routes>
   )
 }
 
