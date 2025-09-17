f5cffd651a9db9ae3db832dd4f41f8673b35c476
JuliusBourbon <naherrrrr@gmail.com>
2025-09-17 19:07:53 +0700

    Add: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import viteLogo from '/vite.svg'
 import { Routes, Route, useLocation  } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/landingPage'
+import Navbar from './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Routes>
-      <Route path='/' element={<LandingPage/>}></Route>
-    </Routes>
+    <div className="w-screen h-screen flex flex-col overflow-x-hidden bg-[linear-gradient(to_bottom_right,#FBBE16,#EEA228,#FBBE16,#F4C75D)]">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}></Route>
+      </Routes>
+    </div>
   )
 }
 
