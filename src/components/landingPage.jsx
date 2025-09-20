fa5616811d1782d7a856f47b455ea2825cf84769
JuliusBourbon <naherrrrr@gmail.com>
2025-09-20 19:58:57 +0700

    Update: Fix UI, Add Multi language text, start button

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -1,19 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { NavLink } from "react-router-dom";
+
 export default function LandingPage(){
+    const myFourSides = [
+        "Hello World",
+        "Halo Dunia",
+        "Konnichiwa Sekai",
+        "Nǐ hǎo shìjiè",
+        "Annyeonghaseyo segye",
+        "Bonjour le monde",
+        "Hallo Welt",
+        "Ciao mondo",
+        "Hola mundo",
+        "Privet mir",
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % myFourSides.length);
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, []);
     return(
         <div className="flex flex-col items-center justify-center w-full h-screen">
             <div className="flex h-full">
-                <div className="w-1/2 flex flex-col items-center justify-center gap-10">
-                    <h1 className="text-5xl w-1/2 font-bold text-[#0A1A6E]">
-                        <span className="text-white">Find Your Word!</span> <br/>
-                        lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt
+                <div className="w-1/2 flex flex-col items-center justify-center">
+                    <h1 className="text-5xl w-1/2 font-bold text-white">
+                        Find Your Word!
                     </h1>
+                    <h1 key={currentIndex} className="text-5xl w-1/2 text-[#FF1000] font-bold animate-pop-up">
+                        {myFourSides[currentIndex]}
+                    </h1>
+                    <h1 className="text-5xl w-1/2 font-bold text-[#0A1A6E] mb-10">lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt</h1>
                     <div className="w-1/2">
-                        <div className="flex items-center justify-between text-2xl py-1 bg-[#0A1A6E] rounded-full w-1/2 text-white font-medium">
+                        <NavLink to='/wordit' className="flex items-center justify-between text-2xl py-1 bg-[#0A1A6E] rounded-full w-1/2 text-white transition-all font-medium hover:translate-y-1.5 hover:bg-[#0A1A6E]/90">
                             <h1 className="ml-5">Start</h1>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mr-5 font-bold">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                             </svg>
-                        </div>
+                        </NavLink>
                     </div>
                 </div>
                 <div className="w-1/2 flex flex-col justify-center items-center gap-5">
@@ -38,7 +65,7 @@ export default function LandingPage(){
             <div className="flex gap-60 text-2xl font-semibold max-h-full my-10 text-[#0A1A6E]">
                 <h1>Indonesia</h1>
                 <h1>English</h1>
-                <h1>Franch</h1>
+                <h1>French</h1>
                 <h1>Italy</h1>
                 <h1>Japan</h1>
                 <h1>Germany</h1>
